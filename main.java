b00028e6f41d497fe33dba76162a99ef0a63887c
Nicholas Seemann <nseemann4740@gmail.com>
2024-08-13 14:50:45 -0400

    Forgot to save file last commit

--- main.java
+++ main.java
@@ -1,9 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-
+import java.util.InputMismatchException;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        Random rand = new Random();
+        ArrayList<Cards> masterList = new ArrayList<Cards>();
+        Integer numCards = 0;
+
         
     }
 }
