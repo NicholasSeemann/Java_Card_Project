7e6f0e0033d9af931444875b2851ded41687340a
Nicholas Seemann <nseemann4740@gmail.com>
2024-08-13 19:10:41 -0400

    Add button and label to GUI

--- GUI.java
+++ GUI.java
@@ -1,21 +1,34 @@
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
-public class GUI {
+public class GUI implements ActionListener{
+
     public GUI(){
         JFrame frame = new JFrame();
+
+        JButton button = new JButton("Click me");
+        JLabel label = new JLabel("Number of clicks: 0");
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Our GUI");
+        frame.setTitle("IST Flashcards");
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void changeLabel(String newLabel){
+
     }
 }
--- main.java
+++ main.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Random;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         Random rand = new Random();
